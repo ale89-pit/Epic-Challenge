94d3211dd0ffdb12a2e4a2b72286047fb13ef779
ale89-pit <72412290+ale89-pit@users.noreply.github.com>
2023-10-16 17:58:49 +0200

    first commit

---
+++ src/main/java/com/biblioTech/EpicChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.biblioTech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpicChallengeApplication.class, args);
+	}
+
+}
