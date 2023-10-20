3711acf1f4419cbd25a69d3b2863d037e87e3103
diclementef92 <diclementef92@gmail.com>
2023-10-20 13:37:49 +0200

    member card state e controller

---
+++ src/main/java/com/biblioTech/Security/controller/MembershipCardController.java
@@ -0,0 +1,18 @@
+package com.biblioTech.Security.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import com.biblioTech.Security.entity.MembershipCard;
+import com.biblioTech.Security.service.MembershipCardService;
+
+@Controller
+public class MembershipCardController {
+	
+	@Autowired MembershipCardService membershipCardService;
+	
+	public MembershipCard save(MembershipCard c) {
+		return membershipCardService.saveMembershipCard(c);
+	}
+
+}
--- src/main/java/com/biblioTech/Security/entity/MembershipCard.java
+++ src/main/java/com/biblioTech/Security/entity/MembershipCard.java
@@ -1,17 +1,12 @@
 package com.biblioTech.Security.entity;
 
-
-import java.util.ArrayList;
-import java.util.List;
-
-import jakarta.annotation.Generated;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -26,6 +21,7 @@ import lombok.NoArgsConstructor;
 public class MembershipCard {
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY )
 	private String id;
 	
 	@ManyToOne(fetch = FetchType.EAGER)
