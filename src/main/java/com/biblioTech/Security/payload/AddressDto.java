198b71fde09c7ab29b22d71cb5d2be54c66495d1
Alessio <72412290+ale89-pit@users.noreply.github.com>
2023-10-23 11:48:03 +0200

    update addressDto

--- src/main/java/com/biblioTech/Security/payload/AddressDto.java
+++ src/main/java/com/biblioTech/Security/payload/AddressDto.java
@@ -1,10 +1,5 @@
 package com.biblioTech.Security.payload;
 
-import java.util.Set;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,13 +12,9 @@ import lombok.ToString;
 @NoArgsConstructor
 @AllArgsConstructor
 public class AddressDto {
-	
-	  	@Column(nullable = false)
-	    String street;
-	    @Column(nullable = false)
-	    String streetNumber;
-	    @ManyToOne(fetch = FetchType.EAGER)
-	    Long municipality;
-	    
+
+	private String street;
+	private String streetNumber;
+	private Long municipality;
 
 }
