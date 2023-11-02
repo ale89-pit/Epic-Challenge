8a2c9e6acc359d9d498d8af536c69f86f4844808
ale89-pit <72412290+ale89-pit@users.noreply.github.com>
2023-11-02 11:52:59 +0100

    jsonIgnore su pass

--- src/main/java/com/biblioTech/Security/entity/User.java
+++ src/main/java/com/biblioTech/Security/entity/User.java
@@ -3,6 +3,8 @@ package com.biblioTech.Security.entity;
 import java.util.HashSet;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -42,6 +44,7 @@ public class User {
 	@Column(nullable = false, unique = true)
 	private String email;
 	@Column(nullable = false)
+	@JsonIgnore
 	private String password;
 
 	@Column
