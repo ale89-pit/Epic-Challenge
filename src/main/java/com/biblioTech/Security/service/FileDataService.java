7e4a9d561654c9b0b7037ccf5a0276bcc68884b6
diclementef92 <diclementef92@gmail.com>
2023-11-03 18:58:36 +0100

    Update FileDataService.java

--- src/main/java/com/biblioTech/Security/service/FileDataService.java
+++ src/main/java/com/biblioTech/Security/service/FileDataService.java
@@ -1,94 +1,88 @@
 package com.biblioTech.Security.service;
 
-import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
-import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
-import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-
+import java.nio.file.StandardCopyOption;
 import java.util.stream.Stream;
 
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Service;
 import org.springframework.util.FileSystemUtils;
-
+import org.springframework.web.multipart.MultipartFile;
 
 import com.biblioTech.Security.repository.FileDataLocal;
 
 @Service
 public class FileDataService implements FileDataLocal {
 
-	
-	
-	private final Path FOLDER_PATH = Paths.get("src","main","resources","csvFiles");
-	
-	
-	//CREA LA DIRECTORY SE NON ESISTE
+	private final Path FOLDER_PATH = Paths.get("src", "main", "resources", "csvFiles");
+
+	// CREA LA DIRECTORY SE NON ESISTE
 	@Override
-	public void init()  {
-		   
-			      try {
-					Files.createDirectories(FOLDER_PATH);
-				} catch (java.io.IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-					throw new RuntimeException("Could not initialize folder for upload!");
-				}	  
+	public void init() {
+
+		try {
+			Files.createDirectories(FOLDER_PATH);
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Could not initialize folder for upload!");
+		}
 	}
-	
-	
+
 	@Override
 	public void save(MultipartFile file) {
-	
+
 		System.out.println(FOLDER_PATH);
-	    try {
-	      Files.copy(file.getInputStream(), this.FOLDER_PATH.resolve(file.getOriginalFilename()));
-	    } catch (Exception e) {
-	      if (e instanceof FileAlreadyExistsException) {
-	        throw new RuntimeException("A file of that name already exists.");
-	      }
+		try {
+			Files.copy(file.getInputStream(), this.FOLDER_PATH.resolve(file.getOriginalFilename()),
+					StandardCopyOption.REPLACE_EXISTING);
+		} catch (IOException e) {
+			throw new RuntimeException(e.getMessage());
+		}
+	}
 
-	      throw new RuntimeException(e.getMessage());
-	    }
-	  }
-	
-	//RITORNA IL PATH ASSOLUTO DAL SERVER
+	// RITORNA IL PATH ASSOLUTO DAL SERVER
 	@Override
-	 public Resource load(String filename) {
-		    try {
-		      Path file = FOLDER_PATH.resolve(filename);
-		      Resource resource = new UrlResource(file.toUri());
+	public Resource load(String filename) {
+		try {
+			Path file = FOLDER_PATH.resolve(filename);
+			Resource resource = new UrlResource(file.toUri());
 
-		      if (resource.exists() || resource.isReadable()) {
-		        return resource;
-		      } else {
-		        throw new RuntimeException("Could not read the file!");
-		      }
-		    } catch (MalformedURLException e) {
-		      throw new RuntimeException("Error: " + e.getMessage());
-		    }
-		  }
-	  public void deleteAll() {
-		    FileSystemUtils.deleteRecursively(FOLDER_PATH.toFile());
-		  }
-	  
-	  //TODO PROVARE A CANCELLARE DOPO
-	  @Override
-	  public Stream<Path> loadAll()  {
-		   
-		      try {
-				return Files.walk(this.FOLDER_PATH, 1).filter(path -> !path.equals(this.FOLDER_PATH)).map(this.FOLDER_PATH::relativize);
-			} catch (java.io.IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-				throw new RuntimeException("Could not load the files!");
+			if (resource.exists() && resource.isReadable()) {
+				return resource;
+			} else {
+				throw new RuntimeException("Could not read the file!");
 			}
-		  
-		   
-		  }
+		} catch (MalformedURLException e) {
+			throw new RuntimeException("Error: " + e.getMessage());
+
+		} catch (InvalidPathException e) {
+			throw new RuntimeException("Error: " + e.getMessage());
+		}
+	}
+
+	public void deleteAll() {
+		FileSystemUtils.deleteRecursively(FOLDER_PATH.toFile());
+	}
+
+	// TODO PROVARE A CANCELLARE DOPO
+	@Override
+	public Stream<Path> loadAll() {
+
+		try {
+			return Files.walk(this.FOLDER_PATH, 1).filter(path -> !path.equals(this.FOLDER_PATH))
+					.map(this.FOLDER_PATH::relativize);
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Could not load the files!");
+		}
+
+	}
 //	public String uploadImageFromFileSystem(MultipartFile file) throws IOException, IllegalStateException, java.io.IOException{
 //		String filePath = FOLDER_PATH + file.getOriginalFilename();
 //		
@@ -103,7 +97,6 @@ public class FileDataService implements FileDataLocal {
 //		return null;
 //	}
 
-	
 //	public byte[] downloadImageFromFileSystem(String fileName) throws java.io.IOException {
 //		Optional<FileData>  fileData = fileDataRepository.findByNome(fileName);
 //		 String filePath = fileData.get().getFilePath();
